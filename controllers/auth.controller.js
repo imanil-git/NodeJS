79e8e0685cd974d5c7fe0c627e172fbfef2d76f3
Anil Rumdali Rai <anil.rai704757@gmail.com>
2025-01-28 20:55:51 +0545

    Connected with MongoDB

---
+++ controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // HASH THE PASSWORD
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  console.log(hashedPassword);
+
+  // CREATE NEW USER AND SAVE TO DB
+
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  console.log(newUser);
+};
+
+export const login = (req, res) => {
+  //db operations
+};
+
+export const logout = (req, res) => {
+  //db operations
+};
