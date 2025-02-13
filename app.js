1f7863f9cc6382b2a472c182109bc2acf32a131a
Anil Rumdali Rai <anil.rai704757@gmail.com>
2025-02-13 17:56:33 +0545

    Trying in frontend

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRouter from "./routes/post.route.js";
-import authRouter from "./routes/auth.route.js"
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
-app.use(cookieParser())
-
-app.use(express.json())
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
+app.use(cookieParser());
+app.use(express.json());
 
 app.use("/api/posts", postRouter);
 app.use("/api/auth", authRouter);
